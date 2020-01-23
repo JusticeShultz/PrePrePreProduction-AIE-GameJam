481aaa47b22cb977b22d286277b427a4b039d336
Joshua Yang <ybd1992@hotmail.com>
2020-01-22 16:49:43 -0800

    basic spawner

---
+++ Assets/Scripts/Joshua/SpawnNotifier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNotifier : MonoBehaviour
+{
+
+    GameObject parentObject;
+
+    void Start()
+    {
+        parentObject = gameObject.transform.parent.gameObject;
+    }
+
+
+    void OnTriggerEnter()
+    {
+        print("called");
+        parentObject.GetComponent<DungeonSpawner>().Spawn(gameObject.name);
+        gameObject.SetActive(false);
+    }
+}
