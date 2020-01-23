090241dfeca950117991a21f92e3ecbc23145824
Justice <shultzjc1@gmail.com>
2020-01-23 15:14:16 -0800

    Door added

---
+++ Assets/Scripts/ArtifactMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtifactMenu : MonoBehaviour
+{
+    public RectTransform target;
+
+    void Update()
+    {
+        //target.SetActive(Input.GetKey(KeyCode.Tab));
+
+        if(Input.GetKey(KeyCode.Tab))
+        {
+            target.anchoredPosition = Vector3.Lerp(target.anchoredPosition, new Vector3(427, 0, 0), 0.07f);
+        }
+        else
+        {
+            target.anchoredPosition = Vector3.Lerp(target.anchoredPosition, new Vector3(-650, 0, 0), 0.07f);
+        }
+    }
+}
